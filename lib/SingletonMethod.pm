0c53ea2af9013928df66916310dc9b6c16424765
Tom Tsuruhara <tom.lpsd@gmail.com>
2010-02-17 10:12:36 +0900

    initial commit

---
+++ lib/SingletonMethod.pm
@@ -0,0 +1,32 @@
+package SingletonMethod;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+SingletonMethod -
+
+=head1 SYNOPSIS
+
+  use SingletonMethod;
+
+=head1 DESCRIPTION
+
+SingletonMethod is
+
+=head1 AUTHOR
+
+Tom Tsuruhara E<lt>tom.lpsd@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
